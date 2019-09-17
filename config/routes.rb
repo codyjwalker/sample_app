ea0782fafa84440ce65c168cdb14730e1ad22ae1
Cody Walker <themightycoder@gmail.com>
2019-09-17 16:11:56 -0700

    Create template of login page with error messages upon invalid login request

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   get     '/about',   to: 'static_pages#about'
   get     '/contact', to: 'static_pages#contact'
   get     '/signup',  to: 'users#new'
-  # This line appears to be deleted in Listing 8.2, however it seems like
-  # it is absolutely required for tests to pass.
-  # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv #
-#  post    '/signup',  to: 'users#create'
-  # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
