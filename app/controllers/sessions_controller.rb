7bb82fdf7f89e5b965ee238a33ac24a0337aa347
Cody Walker <themightycoder@gmail.com>
2019-09-23 19:01:15 -0700

    Implement handling valid login submissions, add new links for logged in users, however COULD NOT GET DROPDOWN TO WORK, SO SIMPLY ADDED THEM ALONGSIDE EXISTING LINKS.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log user in and redirect to user's show page.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination!'
       render 'new'
